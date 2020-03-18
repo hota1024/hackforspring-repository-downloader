c3b4deb61895f4ad25154dde368f65d698d7f586
hota1024 <hotalog@hota1024.com>
2020-03-18 12:18:52 +0900

    🏷️ Add data/repositories.ts

---
+++ data/repositories.ts
@@ -0,0 +1,10 @@
+import { Repository } from '../types/Repository'
+
+// 以下の配列にレッスンのデータを追加してください。
+export const Repositories: Repository[] = [
+  {
+    name: '第10回',
+    description: '第10回のレッスンの内容です。',
+    repository: 'https://github.com/hackforspring/lesson-10'
+  }
+]
