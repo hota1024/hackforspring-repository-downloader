81e31a1183050e13f491cde351df3ec652da1f1e
hota1024 <hotalog@hota1024.com>
2020-03-18 12:25:51 +0900

    Update data/repositoreis

--- data/repositories.ts
+++ data/repositories.ts
@@ -4,7 +4,42 @@ import { Repository } from '../types/Repository'
 export const Repositories: Repository[] = [
   {
     name: '第10回',
-    description: '第10回のレッスンの内容です。',
     repository: 'https://github.com/hackforspring/lesson-10'
+  },
+  {
+    name: '第9回',
+    repository: 'https://github.com/hackforspring/lesson-9'
+  },
+  {
+    name: '第7回',
+    repository: 'https://github.com/hackforspring/lesson-7'
+  },
+  {
+    name: '第6回',
+    repository: 'https://github.com/hackforspring/lesson-6'
+  },
+  {
+    name: '第5回',
+    repository: 'https://github.com/hackforspring/lesson-5'
+  },
+  {
+    name: '第4回',
+    repository: 'https://github.com/hackforspring/lesson-4'
+  },
+  {
+    name: '第3回',
+    repository: 'https://github.com/hackforspring/lesson-3'
+  },
+  {
+    name: '第2回',
+    repository: 'https://github.com/hackforspring/lesson-2'
+  },
+  {
+    name: '第1回',
+    repository: 'https://github.com/hackforspring/lesson-1'
+  },
+  {
+    name: '第0回',
+    repository: 'https://github.com/hackforspring/lesson-0'
   }
 ]
